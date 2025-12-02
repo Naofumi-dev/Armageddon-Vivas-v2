87d7e28a81189a42f02a1ba340d54d605dcac19d
Naofumi-dev <202837949+Naofumi-dev@users.noreply.github.com>
2025-12-02 15:20:59 +0800

    Add Chatbot component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Chatbot from './components/Chatbot';
 
 const App: React.FC = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -33,6 +34,7 @@ const App: React.FC = () => {
       <main>
         <Hero />
       </main>
+      <Chatbot />
     </div>
   );
 };
